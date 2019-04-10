8a53dde18260b96ebc1017c027ae22b5d7c37252
lvyi <1257620619@qq.com>
2019-04-10 23:06:13 +0800

    第一次提交

---
+++ wifi定位/pages/logs/logs.js
@@ -0,0 +1,10 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    lbs:'“WiFi指纹室内定位”是一种可靠的室内定位方案，它成本低易于实现，并且显著提高了定位精度。它通过离线阶段的数据指纹采样，可以大大减少在线匹配的时间，并且解决了多径效应对于定位的影响。'
+  },
+  onLoad: function () {
+  }
+})
